a7e7e9a912da4815c415b19aa30cfb4cde30d4db
HugoChouGT <chaos-zhou@163.com>
2013-01-04 10:05:50 +0800

    Add quit button.

--- practice/hellogtk.c
+++ practice/hellogtk.c
@@ -21,27 +21,27 @@ int main(int argc, char** argv)
 {
     GtkWidget *window;
     GtkWidget *button;
-    GtkWidget *box1;
+    GtkWidget *table;
 
     gtk_init(&argc, &argv);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Hello Buttons!");
+    gtk_window_set_title(GTK_WINDOW(window), "Table");
 
     g_signal_connect(G_OBJECT(window), "delete_event", G_CALLBACK(delete_event), NULL);
 
 //    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
 
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 20);
 
-    box1 = gtk_hbox_new(FALSE, 0);
-    gtk_container_add(GTK_CONTAINER(window), box1);
+    table = gtk_table_new(2, 2, TRUE);
+    gtk_container_add(GTK_CONTAINER(window), table);
 
     button = gtk_button_new_with_label("Button 1");
 
     g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(callback), "button 1");
 
-    gtk_box_pack_start(GTK_BOX(box1), button, TRUE, TRUE, 0);
+    gtk_table_attach_defaults(GTK_TABLE(table), button, 0, 1, 0, 1);
 
     gtk_widget_show(button);
 
@@ -49,11 +49,19 @@ int main(int argc, char** argv)
 
     g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(callback), "button 2");
 
-    gtk_box_pack_start(GTK_BOX(box1), button, TRUE, TRUE, 0);
+    gtk_table_attach_defaults(GTK_TABLE(table), button, 1, 2, 0, 1);
 
     gtk_widget_show(button);
 
-    gtk_widget_show(box1);
+    button = gtk_button_new_with_label("Quit");
+
+    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(delete_event), NULL);
+
+    gtk_table_attach_defaults(GTK_TABLE(table), button, 0, 2, 1, 2);
+
+    gtk_widget_show(button);
+
+    gtk_widget_show(table);
     gtk_widget_show(window);
 
     gtk_main();
